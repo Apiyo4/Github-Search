7b9910712e18d08ed37a150cc8731aba7ee7503b
Annette Odingo <odingoannette@gmail.com>
2019-06-09 17:32:20 +0300

    Add style to app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,6 @@ export class AppComponent {
   constructor(private searchService:SearchService ){
     this.searchService.getUser().subscribe(user => {
     this.user = user;
-    console.log(this.user)
   })
   }
 
